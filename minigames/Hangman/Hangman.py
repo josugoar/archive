a4f4166be566effe664c1cb009745c92977c22fe
Josh <56826352+JoshGoA@users.noreply.github.com>
2019-11-16 18:06:56 +0100

    Update Hangman.py

--- minigames/Hangman/Hangman.py
+++ minigames/Hangman/Hangman.py
@@ -1,45 +1,57 @@
 import os
+import time
 import random
-
-
-def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+import string
 
 db = ["hello", "word"]
-word_select = db[random.randint(0, len(db) - 1)].lower()
-
-def hang(max_guess):
-
-    cls()
-    input("HANGMAN")
-
-    word = len(word_select) * "_"
-    correct = 0
-
-    for i in range(0, max_guess):
-        output = ""
-        cls()
-        print(word + "\n")
-        print("Guesses left: " + str(max_guess-i))
-        guess = input("Enter guess: ").lower()
-        for char in range(0, len(word_select)):
-            if (word[char] == "_"):
-                if (word_select[char] == guess):
-                    output += guess
-                    correct += 1
-                    max_guess += 1
-                else:
-                    output += "_"
+def cls():
+    os.system("cls")
+def checkLetter(guess, word, output, correct=None):
+    temp_output = ""
+    print("Guess: " + guess)
+    for char in range(0, len(word)):
+        if (output[char] == "_"):
+            if (guess == word[char]):
+                print("\33[92m" + guess + "\033[0m", end="", flush=True)
+                temp_output += guess
+                correct += 1
             else:
-                output += word[char]
-        word = output
-        if (correct == len(word_select)):
-            cls()
-            print(word + "\n\nYou Win!")
-            return 1
-
+                print("\33[91m" + "_" + "\033[0m", end="", flush=True)
+                temp_output += "_"
+        else:
+            print("\33[92m" + output[char] + "\033[0m", end="", flush=True)
+            temp_output += output[char]
+        time.sleep(0.25)
+    return temp_output, correct
+def hang(max_guess):
     cls()
-    print("Word: " + word_select + "\n\nYou Lose...")
+    print("\33[44m" + "HANGMAN" + "\033[0m")
+    input("\33[5m" + "Press enter to continue" + "\033[0m")
+    word = random.choice(db).upper()
+    output = len(word) * "_"
+    correct = 0
+    incorrect = 0
+    while incorrect < max_guess:
+        cls()
+        if (correct == len(word)):
+            print("\33[42m" + "You win!" + "\033[0m")
+            return 1
+        print("{}\nGuesses left: {}".format(output, max_guess - incorrect))
+        guess = input("Enter guess: ").upper()
+        cls()
+        if guess.isalpha():
+            if guess in word:
+                output, correct = checkLetter(guess, word, output, correct)
+                print("\033[92m" + "\nCorrect!" + "\033[0m")
+            else:
+                checkLetter(guess, word, output)
+                print("\033[91m" + "\nIncorrect..." + "\033[0m")
+                incorrect += 1
+        else:
+            print("\33[91m" + "Error: input must be a letter" + "\033[0m")
+        time.sleep(1)
+    time.sleep(1)
+    cls()
+    print("\33[41m" + "You loose..." + "\033[0m")
     return 0
-
 hang(max_guess=5)
