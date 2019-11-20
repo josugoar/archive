11d1bd25d249e1d080c11df50555e5599b18253b
Josh <56826352+JoshGoA@users.noreply.github.com>
2019-11-20 20:16:59 +0100

    Add files via upload

--- kNN/KNN.py
+++ kNN/KNN.py
@@ -3,48 +3,77 @@ import pandas
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
-k = 3
+# Read data
 data = pandas.read_csv("_KNN/data.csv")
-point = [1.190041585755129194e+00, 6.612370213365792893e+00]
 
-dist: dict = {}
-for i in range(0, len(data)):
-    if data["x"][i] != point[0] and data["y"][i] != point[1]:
+# Order data from closest to furthest of distance between points
+def order(point):
+    dist: dict = {}
+    for i in range(0, len(data)):
         dist[(data["x"][i], data["y"][i])] = math.sqrt((point[0] - data["x"][i]) ** 2 + (point[1] - data["y"][i]) ** 2)
-dist = dict(OrderedDict(sorted(dist.items(), key=lambda x: x[1])))
-keys = list(dist.keys())
-vals = list(dist.values())
+    dist = OrderedDict(sorted(dist.items(), key=lambda x: x[1]))
+    return list(dist.keys())
 
-middle = (data["x"].max() + data["x"].min()) / 2
-data_lower: dict = {}
-data_upper: dict = {}
-for i in range(0, len(data)):
-	if data["x"][i] != point[0] and data["y"][i] != point[1]:
-		if data["x"][i] < middle:
-			data_lower[data["x"][i]] = data["y"][i]
-		elif data["x"][i] > middle:
-			data_upper[data["x"][i]] = data["y"][i]
+# Define separation criteria (red and blue points)
+def separation(point, criteria):
+    data_lower: dict = {}
+    data_upper: dict = {}
+    for i in range(0, len(data)):
+        if data["x"][i] < criteria:
+            data_lower[data["x"][i]] = data["y"][i]
+        elif data["x"][i] > criteria:
+            data_upper[data["x"][i]] = data["y"][i]
+    return data_lower, data_upper
 
-nearest: dict = {}
-for i in range(0, k):
-	nearest[keys[i][0]] = keys[i][1]
+# Get k nearest neighbors
+def near(keys, k):
+    nearest: dict = {}
+    for i in range(0, k):
+        nearest[keys[i][0]] = keys[i][1]
+    return nearest
 
-lower = 0
-upper = 0
-for i in range(0, len(nearest)):
-	if list(nearest.keys())[i] < middle:
-		lower += 1
-	elif list(nearest.keys())[i] < middle:
-		upper += 1
-if lower > upper:
-	print("Point is BLUE")
-elif lower < upper:
-	print("Point is RED")
+# Define selected point to analize color depending on neighbors
+def area(nearest, middle):
+    lower = 0
+    upper = 0
+    for i in range(0, len(nearest)):
+        if list(nearest.keys())[i] < middle:
+            lower += 1
+        elif list(nearest.keys())[i] > middle:
+            upper += 1
+    if lower > upper:
+        print("Point is BLUE")
+        return "blue"
+    elif lower < upper:
+        print("Point is RED")
+        return "red"
 
-fig, ax = plt.subplots()
-ax.axis("on")
-ax.scatter(data_lower.keys(), data_lower.values(), color="#0000FF", alpha=0.5)
-ax.scatter(data_upper.keys(), data_upper.values(), color="#FF0000", alpha=0.5)
-ax.scatter(nearest.keys(), nearest.values(), color="#000000")
-ax.scatter(point[0], point[1], color="#00FF00")
-plt.show()
+# Plot data
+def grid(data_lower, data_upper, point, nearest):
+    fig, ax = plt.subplots()
+    ax.axis("on")
+    ax.scatter(data_lower.keys(), data_lower.values(), color="#0000FF", alpha=0.5)
+    ax.scatter(data_upper.keys(), data_upper.values(), color="#FF0000", alpha=0.5)
+    ax.scatter(nearest.keys(), nearest.values(), color="#000000", s=2.5)
+    ax.scatter(point[0], point[1], color="#00FF00", s=2.5)
+    plt.show()
+
+# Main
+def main(k, point, plot=False):
+    if point[0] in data["x"] and point[1] in data["y"]:
+        print("Point already exists...")
+        return 0
+    middleX = (data["x"].max() + data["x"].min()) / 2
+    middleY = (data["y"].max() + data["y"].min()) / 2
+    coordinates = order(point)
+    data_lower, data_upper = separation(point, middleX)
+    nearest = near(coordinates, k)
+    area(nearest, middleX)
+    if plot:
+        grid(data_lower, data_upper, point, nearest)
+
+# k: nearest neighbor number
+# point: point to analize
+# plot: show grid
+if __name__ == "__main__":
+    main(k=100, point=[1.5, 8.5], plot=True)
