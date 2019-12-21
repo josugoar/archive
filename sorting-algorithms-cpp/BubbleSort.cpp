74510d3c5417482ea449f8a91c7aa6ef24c958ab
Josh <56826352+JoshGoA@users.noreply.github.com>
2019-12-21 18:00:42 +0100

    Improve comments

--- sorting-algorithms-cpp/BubbleSort.cpp
+++ sorting-algorithms-cpp/BubbleSort.cpp
@@ -1,60 +1,51 @@
-//Include:
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-//Namespace:
 using namespace std;
 using namespace sf;
 
-//Function main:
 int main() {
-
-	//Render window:
+	// Render window
 	RenderWindow window(VideoMode(509, 250), "BubbleSortVisualized", Style::Close);
-
-	//Window open:
+	// Window open
 	while (window.isOpen()) {
-
-		//Define event:
+		// Define event
 		Event evnt;
 		while (window.pollEvent(evnt)) {
-			//Window background:
+			// Window background
 			window.clear(Color::Black);
 			window.display();
-			//Event type:
+			// Event type
 			switch (evnt.type) {
-				//Window close:
+			// Window close
 			case Event::Closed:
 				window.close();
 				break;
 			}
 		}
-
-		//Define values:
-		int i; int x; int j; int temp;	//Iterations
+		// Define values
+		int i; int x; int j; int temp;		    //Iterations
 		int column[101];			    //Column array
 		srand(time(NULL));			    //Initialize random value to null
-
-		//Draw unsorted values:
+		// Draw unsorted values
 		for (i = 1; i < 101; i++) {
 			int r = rand() % 100;
 			column[i] = r;
-			//White line:
+			// White line
 			RectangleShape line1(Vector2f(9, 250));
 			line1.setFillColor(Color::White);
-			//Black line:
+			// Black line
 			RectangleShape line2(Vector2f(9, 250 - r));
 			line2.setFillColor(Color::Black);
-			//Set position:
+			// Set position
 			line1.setPosition(5 * (i - 1), 0);
 			line2.setPosition(5 * (i - 1), 0);
-			//Draw line:
+			// Draw line
 			window.draw(line1);
 			window.draw(line2);
 			window.display();
 		}
-
-		//Print unsorted values:
+		// Print unsorted values
 		cout << "Unsorted values:\n";
 		for (i = 1; i < 101; i++) {
 			cout << column[i] << " ";
@@ -62,12 +53,10 @@ int main() {
 				cout << "\n";
 			}
 		}
-
-		//Keyboard input required to proceed:
+		// Keyboard input required to proceed
 		system("pause");
 		cout << "\n\n";
-
-		//Bubble sort:
+		// Bubble sort
 		for (i = 1; i < 101; i++) {
 			for (j = 1; j < 101 - i; j++) {
 				if (column[j] > column[j + 1]) {
@@ -76,26 +65,24 @@ int main() {
 					column[j + 1] = temp;
 				}
 			}
-
-			//Draw sorted values:
+			// Draw sorted values
 			for (x = 1; x < 101; ++x) {
-				//White line:
+				// White line
 				RectangleShape line1(Vector2f(9, 250));
 				line1.setFillColor(Color::White);
-				//Black line:
+				// Black line
 				RectangleShape line2(Vector2f(9, 250 - column[x]));
 				line2.setFillColor(Color::Black);
-				//Set position:
+				// Set position
 				line1.setPosition(5 * (x - 1), 0);
 				line2.setPosition(5 * (x - 1), 0);
-				//Draw line:
+				// Draw line
 				window.draw(line1);
 				window.draw(line2);
 				window.display();
 			}
 		}
-
-		//Print sorted values
+		// Print sorted values
 		cout << "Sorted values:\n";
 		for (i = 1; i < 101; i++) {
 			cout << column[i] << " ";
@@ -103,11 +90,9 @@ int main() {
 				cout << "\n";
 			}
 		}
-
-		//End:
+		// End
 		break;
 	}
-
-	//Pause:
+	// Pause
 	system("pause");
 }
