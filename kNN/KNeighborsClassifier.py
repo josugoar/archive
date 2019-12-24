de380fa1c8d66dc5a86072b49494df707c870b87
Josh <56826352+JoshGoA@users.noreply.github.com>
2019-12-24 22:08:28 +0100

    Add files via upload

--- kNN/KNeighborsClassifier.py
+++ kNN/KNeighborsClassifier.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from scipy.stats import mode
 
 class KNeighborsClassifier:
     """
@@ -16,16 +15,21 @@ class KNeighborsClassifier:
     fit_called = False
 
     def __new__(cls, n_neighbors=5):
-        # Assert data type
+        # Assert data type and value
         assert isinstance(n_neighbors, int), 'Data Type Error: n_neighbors dtype must be integer'
-        return super(KNeighborsClassifier, cls).__new__(cls)
+        assert n_neighbors > 0, 'Value Error: n_neighbors must be a positive integer'
+
+        # Create instance
+        instance = super(KNeighborsClassifier, cls).__new__(cls)
+
+        return instance
 
     def __init__(self, n_neighbors=5):
         self.n_neighbors = n_neighbors
 
     def __str__(self):
         # Return class atributes
-        return f'KNeighborsClassifier\n- n_neighbors: {self.n_neighbors}'
+        return f'KNeighborsClassifier\n  n_neighbors: {self.n_neighbors}'
 
     def fit(self, X, y):
         """
@@ -40,14 +44,14 @@ class KNeighborsClassifier:
         assert X.ndim == 2, 'Dimension Error: X.shape must be two-dimensional'
         assert y.ndim == 1, 'Dimension Error: y.shape must be one-dimensional'
         assert X.shape[0] == y.size, 'Dimension Error: X and y shapes must respectively be of [n_samples, m_features] and [n_samples,]'
-        assert self.n_neighbors > X.shape[0], 'Dimension Error: n_neighbors must not be larger than X n_rows'
+        assert X.shape[0] > self.n_neighbors, 'Dimension Error: n_neighbors must not be larger than X n_rows'
 
         # Activate KNeighborsClassifier.fit() method flag
         KNeighborsClassifier.fit_called = True
 
         # Fit X and y to class
-        self.__X = X
-        self.__y = y
+        self.__X_fit = X
+        self.__y_fit = y
 
     def predict(self, X):
         """
@@ -63,16 +67,16 @@ class KNeighborsClassifier:
         # Assert method and dimensions
         assert KNeighborsClassifier.fit_called, 'Method Error: KNeighborsClassifier.fit() method must first be called'
         assert X.ndim == 2, 'Dimension Error: X.shape must be two-dimensional'
-        assert self.__X.shape[1] == X.shape[1], 'Dimension Error: fit X and input X shapes must respectively be of [n_samples, m_features] and [n_query, m_features]'
+        assert self.__X_fit.shape[1] == X.shape[1], 'Dimension Error: fit X and input X shapes must respectively be of [n_samples, m_features] and [n_query, m_features]'
 
         # Fit point to point k distances an indices
-        self.__distances, self.__indices = self.kneighbors(X)
-        # Set point labels
-        self.__labels = np.array([mode(self.__y[indice])[0][0] for indice in self.__indices])
+        indices = self.kneighbors(X)
+        # Find most common label
+        labels = np.array([np.unique(self.__y_fit[indice])[0] for indice in indices])
 
-        return self.__labels
+        return labels
 
-    def kneighbors(self, X, return_distances=True):
+    def kneighbors(self, X, return_distances=False):
         """
         Finds the K-neighbors of a point and returns indices of and distances to the neighbors of each point
 
@@ -92,14 +96,14 @@ class KNeighborsClassifier:
         X = X[:, :, np.newaxis]
 
         # Calculate point to point distances
-        dist = np.sqrt((np.power(X - self.__X.T, 2)).sum(axis=1))
+        dist = np.sqrt((np.power(X - self.__X_fit.T, 2)).sum(axis=1))
 
         # Calculate point to point k indices
-        kindices = np.argsort(dist, axis=1)[:, 0:self.n_neighbors]
+        kindices = np.argsort(dist, axis=1)[:, :self.n_neighbors]
 
-        if return_distances == True:
+        if return_distances:
             # Calculate point to point k distances
-            kdistances = np.sort(dist, axis=1)[:, 0:self.n_neighbors]
+            kdistances = np.sort(dist, axis=1)[:, :self.n_neighbors]
             return kdistances, kindices
         return kindices
 
@@ -118,10 +122,10 @@ class KNeighborsClassifier:
         # Assert dimensions
         assert X.shape[0] == y_true.size, 'Dimension Error: X and y shapes must respectively be of [n_query, m_features] and [n_query,]'
 
-        y_pred = self.predict(X)
-
         # Calculate mean score
-        return np.sum(y_pred == y_true)/y_true.size
+        score = np.sum(self.predict(X) == y_true) / y_true.size
+
+        return score
 
     def kneighbors_2Dgraph(self, X):
         """
@@ -137,40 +141,43 @@ class KNeighborsClassifier:
         # Assert dimension
         assert X.shape[1] == 2, 'Dimension Error: X must not have more than two features'
 
-        # Predict labels
-        labels = self.predict(X)
-
-        # Plot parameters
+        # Plot style
         plt.style.use('seaborn')
-        plt.title('kneighbors_2Dgraph')
-        plt.xlabel('feature_1')
-        plt.ylabel('feature_2')
-        plt.xlim(self.__X[:, 0].min(), self.__X[:, 0].max())
-        plt.ylim(self.__X[:, 1].min(), self.__X[:, 1].max())
+
+        # Labels
+        labels_pred = self.predict(X)
+        labels_true = np.unique(self.__y_fit)
+
+        # Plot points
+        for label in labels_true:
+
+            # True labels
+            mask_true = np.argwhere(self.__y_fit == label)
+            fit_point = plt.scatter(self.__X_fit[mask_true, 0], self.__X_fit[mask_true, 1],
+                                    alpha=0.5)
+
+            # Predicted labels
+            mask_pred = np.argwhere(labels_pred == label)
+            pred_point = plt.scatter(X[mask_pred, 0], X[mask_pred, 1],
+                                     color=fit_point.get_facecolor()[0],
+                                     alpha=1)
 
         # Make grid
-        xx, yy = np.meshgrid(np.linspace(self.__X[:, 0].min(), self.__X[:, 0].max(), num=200),
-                             np.linspace(self.__X[:, 1].min(), self.__X[:, 1].max(), num=200))
+        num = 200
+        xx, yy = np.meshgrid(np.linspace(*plt.xlim(), num),
+                             np.linspace(*plt.ylim(), num))
         Z = self.predict(np.c_[xx.ravel(), yy.ravel()]).reshape(xx.shape)
 
         # Plot contourn
         contours = plt.contourf(xx, yy, Z,
-                                alpha=0.3,
-                                levels=np.arange(np.unique(self.__y).size + 1) - 0.5)
+                                alpha=0.25,
+                                levels=np.arange(labels_true.size + 1) - 0.5,
+                                cmap='rainbow')
 
-        # Plot points
-        for label in np.unique(self.__y):
-
-            # True labels
-            mask_true = np.argwhere(self.__y == label)
-            fit_point = plt.scatter(self.__X[mask_true, 0], self.__X[mask_true, 1],
-                                    alpha=0.5)
-
-            # Predicted labels
-            mask_pred = np.argwhere(labels == label)
-            pred_point = plt.scatter(X[mask_pred, 0], X[mask_pred, 1],
-                                     color=fit_point.get_facecolor()[0],
-                                     alpha=1)
+        # Plot parameters
+        plt.title('kneighbors_2Dgraph')
+        plt.xlabel('feature_1')
+        plt.ylabel('feature_2')
 
         # Show plot
         plt.show()
