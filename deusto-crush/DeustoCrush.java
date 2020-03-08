91da3803b5f6930c18e301754ec5ddd5d69aab00
JoshGoA <josugoar@gmail.com>
2020-03-08 13:43:04 +0100

    Updated GUI

--- deusto-crush/DeustoCrush.java
+++ deusto-crush/DeustoCrush.java
@@ -1,34 +1,52 @@
 package _DeustoCrush;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.Point;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
-import java.util.stream.Stream;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
 
 /**
  * DeustoCrush
+ *
  * @author JoshGoA (ansi rectangle idea by nullx)
  */
-public class DeustoCrush {
+public class DeustoCrush extends JFrame implements Runnable {
+
+    private static final long serialVersionUID = 1L;
 
     private static final int GRID_N = 6;
     private static final int MIN_STREAK = 3;
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static final int DELAY = 500;
+
+    private final JPanel panel = new JPanel(new GridLayout(GRID_N, GRID_N, 0, 0));
 
     private int emptyMoves = 0;
     private int score = 0;
-    protected Candy[][] grid;
+    private Candy[][] grid;
+    private Point point = null;
 
     {
+        // Randomize grid on start
         this.randomize();
     }
 
     public static final void main(final String[] args) {
-        new DeustoCrush().run();
+        // Invoke Runnable
+        EventQueue.invokeLater(new DeustoCrush());
     }
 
     @Override
@@ -44,86 +62,80 @@ public class DeustoCrush {
             out += i;
             for (int j = 0; j < GRID_N; j++) {
                 // Candy
-                out += String.format(" %s", this.getGrid()[i][j]);
+                out += String.format(" %s", this.grid[i][j]);
             }
             out += "\n";
         }
         return out;
     }
 
-    private final void run() {
-        while (this.emptyMoves < 5) {
-            // Print grid
-            System.out.println(this);
-            // Advance iter
-            this.iterWrapper();
-            // Read user input
-            String[] input;
-            try {
-                System.out.println(String.format("Score: %d", this.score));
-                System.out.print("Enter movement: ");
-                input = br.readLine().split(" ");
-                if (input.length != 2) {
-                    throw new Exception();
-                }
-            } catch (final Exception e) {
-                System.out.println("Invalid input");
-                continue;
-            } finally {
-                System.out.println("");
-            }
-            // Store user input in integer array
-            final int[][] points = new int[2][2];
-            for (int i = 0; i < 2; i++) {
-                points[i] = Stream.of(input[i].split("-")).mapToInt(Integer::parseInt).toArray();
-            }
-            // Swap input points
-            if (!this.swap(this.getGrid()[points[0][0]][points[0][1]], this.getGrid()[points[1][0]][points[1][1]])) {
-                System.out.println("Invalid neighboring points\n");
-            }
-        }
-        System.out.println("Too many empty moves");
-    }
-
+    /**
+     * Randomize grid
+     */
     private final void randomize() {
         // Create new empty grid
         this.grid = new Candy[GRID_N][GRID_N];
         // Fill grid with randomly colored candies
         for (int i = 0; i < GRID_N; i++) {
             for (int j = 0; j < GRID_N; j++) {
-                grid[i][j] = new Candy(new Point(i, j));
+                final Candy candy = new Candy(new Point(i, j));
+                candy.addMouseListener(candy.new CandyListener());
+                grid[i][j] = candy;
+                this.panel.add(candy);
             }
         }
     }
 
-    private final boolean swap(final Candy c1, final Candy c2) {
+    @Override
+    public final void run() {
+        this.panel.setPreferredSize(new Dimension(250, 250));
+        this.setContentPane(panel);
+        this.pack();
+        this.setTitle("DeustoCrush");
+        this.setVisible(true);
+        this.setResizable(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.iterWrapper();
+    }
+
+    /**
+     * Swap two Candy
+     *
+     * @param c1
+     * @param c2
+     */
+    private final void swap(final Candy c1, final Candy c2) {
         // Range through four neighbors
         for (int i = c1.getSeed().x - 1; i <= c1.getSeed().x + 1; i++) {
             for (int j = c1.getSeed().y + Math.abs(c1.getSeed().x - i) - 1; j <= c1.getSeed().y + 1; j += 2) {
                 // Check for array index out of bounds
                 if (i >= 0 && i < GRID_N && j >= 0 && j < GRID_N) {
                     // Check for neighboring candies
-                    if (this.getGrid()[i][j] == c2) {
+                    if (this.grid[i][j] == c2) {
                         // Swap colors but not seeds
-                        final String tempColor = this.getGrid()[i][j].getColor();
-                        this.getGrid()[i][j].setColor(c1.color);
+                        final String tempColor = this.grid[i][j].getColor();
+                        this.grid[i][j].setColor(c1.color);
                         c1.setColor(tempColor);
-                        return true;
                     }
                 }
             }
         }
-        return false;
     }
 
+    /**
+     * Check for Candy in streak
+     *
+     * @param c
+     * @return
+     */
     private final List<List<Candy>> check(final Candy c) {
         final List<List<Candy>> inStreak = new ArrayList<List<Candy>>();
         // Check horizontal streaks
         int streak = 0;
         for (int i = 0; i < GRID_N; i++) {
             // Reset streak
-            if (this.getGrid()[c.getSeed().x][c.getSeed().y].getColor() != this.getGrid()[i][c.getSeed().y]
-                    .getColor()) {
+            if (this.grid[c.getSeed().x][c.getSeed().y].getColor() != this.grid[i][c.getSeed().y].getColor()) {
                 streak = 0;
                 continue;
             }
@@ -133,19 +145,18 @@ public class DeustoCrush {
                 // Add new streak
                 inStreak.add(new ArrayList<Candy>());
                 for (int j = 0; j < MIN_STREAK; j++) {
-                    inStreak.get(inStreak.size() - 1).add(this.getGrid()[i - j][c.getSeed().y]);
+                    inStreak.get(inStreak.size() - 1).add(this.grid[i - j][c.getSeed().y]);
                 }
             } else if (streak > MIN_STREAK) {
                 // Add to existing streak
-                inStreak.get(inStreak.size() - 1).add(this.getGrid()[i][c.getSeed().y]);
+                inStreak.get(inStreak.size() - 1).add(this.grid[i][c.getSeed().y]);
             }
         }
         // Check vertical streaks
         streak = 0;
         for (int i = 0; i < GRID_N; i++) {
             // Reset streak
-            if (this.getGrid()[c.getSeed().x][c.getSeed().y].getColor() != this.getGrid()[c.getSeed().x][i]
-                    .getColor()) {
+            if (this.grid[c.getSeed().x][c.getSeed().y].getColor() != this.grid[c.getSeed().x][i].getColor()) {
                 streak = 0;
                 continue;
             }
@@ -155,23 +166,28 @@ public class DeustoCrush {
                 // Add new streak
                 inStreak.add(new ArrayList<Candy>());
                 for (int j = 0; j < MIN_STREAK; j++) {
-                    inStreak.get(inStreak.size() - 1).add(this.getGrid()[c.getSeed().x][i - j]);
+                    inStreak.get(inStreak.size() - 1).add(this.grid[c.getSeed().x][i - j]);
                 }
             } else if (streak > MIN_STREAK) {
                 // Add to existing streak
-                inStreak.get(inStreak.size() - 1).add(this.getGrid()[c.getSeed().x][i]);
+                inStreak.get(inStreak.size() - 1).add(this.grid[c.getSeed().x][i]);
             }
         }
         // Use fori instead of foreach to avoid ConcurrentModificationException
         for (int i = 0; i < inStreak.size(); i++) {
             // Check streaks containing candy
-            if (!inStreak.get(i).contains(this.getGrid()[c.getSeed().x][c.getSeed().y])) {
+            if (!inStreak.get(i).contains(this.grid[c.getSeed().x][c.getSeed().y])) {
                 inStreak.remove(inStreak.get(i));
             }
         }
         return inStreak;
     }
 
+    /**
+     * Calculate streak points
+     *
+     * @param inStreak
+     */
     private final void calcPoints(final List<List<Candy>> inStreak) {
         // Check for existing streak
         if (inStreak != null) {
@@ -186,6 +202,12 @@ public class DeustoCrush {
         }
     }
 
+    /**
+     * Delete Candy in streak
+     *
+     * @param inStreak
+     * @return
+     */
     private final boolean delete(final List<List<Candy>> inStreak) {
         // Deleted flag
         boolean deleted = false;
@@ -203,22 +225,36 @@ public class DeustoCrush {
         return deleted;
     }
 
+    /**
+     * Advance Candy vertically
+     */
     private final void advance() {
         // Range through each candy
         for (int i = 0; i < GRID_N; i++) {
             for (int j = 0; j < GRID_N; j++) {
                 // Check for deleted candy
-                if (this.getGrid()[i][j].getColor() == null) {
+                if (this.grid[i][j].getColor() == null) {
                     // Redefine leading row candies
-                    if (this.getGrid()[i][j].getSeed().x == 0) {
-                        this.getGrid()[i][j] = new Candy(this.getGrid()[i][j].getSeed());
+                    if (this.grid[i][j].getSeed().x == 0) {
+                        this.grid[i][j].setColor(Candy.ANSI_COLORS[new Random().nextInt(Candy.ANSI_COLORS.length)]);
                     } else {
                         // Swap candy with top candy
-                        swap(this.getGrid()[i][j], this.getGrid()[i - 1][j]);
+                        swap(this.grid[i][j], this.grid[i - 1][j]);
                         // Recursively call method until convergence
                         advance();
                     }
                 }
+            }
+        }
+    }
+
+    /**
+     * Repaint all Candy
+     */
+    private final void paint() {
+        for (final Candy[] candies : this.grid) {
+            for (final Candy candy : candies) {
+                candy.repaint();
             }
         }
     }
@@ -230,7 +266,7 @@ public class DeustoCrush {
             for (int j = 0; j < GRID_N; j++) {
                 // Check for streak and delete it
                 if (!deleted) {
-                    final List<List<Candy>> inStreak = this.check(this.getGrid()[i][j]);
+                    final List<List<Candy>> inStreak = this.check(this.grid[i][j]);
                     this.calcPoints(inStreak);
                     deleted = this.delete(inStreak);
                 }
@@ -241,7 +277,13 @@ public class DeustoCrush {
         // Delete new streaks if they appear
         if (deleted) {
             System.out.println(this);
-            this.iterWrapper();
+            System.out.println("Score: " + this.score);
+            // Set delay for repaint
+            new Timer(DeustoCrush.DELAY, e -> {
+                this.paint();
+                this.iterWrapper();
+                ((Timer) e.getSource()).stop();
+            }).start();
         }
     }
 
@@ -249,7 +291,20 @@ public class DeustoCrush {
         return this.grid;
     }
 
-    private static final class Candy {
+    public final Point getPoint() {
+        return this.point;
+    }
+
+    public final void setPoint(final Point point) {
+        this.point = point;
+    }
+
+    /**
+     * Candy
+     */
+    private static final class Candy extends JPanel {
+
+        private static final long serialVersionUID = 1L;
 
         // Color strings
         public static final String[] ANSI_COLORS = { "\u001B[31m", "\u001B[32m", "\u001B[34m", "\u001B[35m" };
@@ -258,8 +313,37 @@ public class DeustoCrush {
         private String color = ANSI_COLORS[new Random().nextInt(ANSI_COLORS.length)];
         private Point seed;
 
+        {
+            this.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        }
+
         public Candy(final Point candySeed) {
             this.setSeed(candySeed);
+        }
+
+        @Override
+        public final void paintComponent(final Graphics g) {
+            Color c;
+            // Paint Cell depending on Color
+            if (this.color == null) {
+                c = Color.WHITE;
+            } else {
+                switch (this.color) {
+                    case "\u001B[31m":
+                        c = Color.RED;
+                        break;
+                    case "\u001B[32m":
+                        c = Color.GREEN;
+                        break;
+                    case "\u001B[34m":
+                        c = Color.BLUE;
+                        break;
+                    default:
+                        c = Color.MAGENTA;
+                }
+            }
+            g.setColor(c);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
         }
 
         @Override
@@ -282,6 +366,46 @@ public class DeustoCrush {
 
         public final void setSeed(final Point seed) {
             this.seed = Objects.requireNonNull(seed, "'seed' must not be null");
+        }
+
+        /**
+         * Candy Listener
+         */
+        private final class CandyListener extends MouseAdapter {
+
+            @Override
+            public void mousePressed(final MouseEvent e) throws NullPointerException {
+                // Get frame
+                final DeustoCrush frame = (DeustoCrush) SwingUtilities.getWindowAncestor(Candy.this);
+                // Check if Candy is selected
+                if (frame.getPoint() == null) {
+                    frame.setPoint(Candy.this.getSeed());
+                } else {
+                    // Check for too many empty moves
+                    if (frame.emptyMoves >= 5) {
+                        System.out.println("Too many empty moves...");
+                        try {
+                            Thread.sleep(1000000);
+                        } catch (InterruptedException e1) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                    // Swap Candy
+                    frame.swap(frame.grid[frame.getPoint().x][frame.getPoint().y], Candy.this);
+                    // Increase empty moves if no Candy have been deleted
+                    if (!frame.delete(frame.check(frame.grid[frame.getPoint().x][frame.getPoint().y]))
+                            && !frame.delete(frame.check(Candy.this))) {
+                        frame.emptyMoves++;
+                    }
+                    // Repaint Candy
+                    frame.paint();
+                    // Reset selection
+                    frame.setPoint(null);
+                    // Check for more streaks
+                    frame.iterWrapper();
+                }
+            }
+
         }
 
     }
