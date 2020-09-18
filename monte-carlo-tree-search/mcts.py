f37bee9d1f7eaea57bac7fa3f0ee5dcd64803ecd
Josh <56826352+JoshGoA@users.noreply.github.com>
2020-09-18 18:47:21 +0200

    Update mcts.py

--- monte-carlo-tree-search/mcts.py
+++ monte-carlo-tree-search/mcts.py
@@ -4,43 +4,36 @@ import math
 import random
 
 
-def upper_confidence_trees(node, child, exploration_weight=math.sqrt(2)):
-    return (child.total_reward / child.visits + exploration_weight
+def uct(node, child, exploration_weight=math.sqrt(2)):
+    return (child.exploitation_component + exploration_weight
             * math.sqrt(math.log(node.visits) / child.visits))
 
 
-def uniform_random_distribution(state):
-    return random.choice(state.actions)
-
-
 class MCTS:
-    
-    def __init__(self, bandit_strategy=upper_confidence_trees,
-                 rollout_policy=uniform_random_distribution):
+
+    def __init__(self, bandit_strategy=uct):
         self.bandit_strategy = bandit_strategy
-        self.rollout_policy = rollout_policy
 
     def search(self, root):
-        leaf = self._select(root)
-        reward = self._rollout(leaf.state)
+        leaf = self._tree_policy(root)
+        reward = self._default_policy(leaf.state)
         self._backpropagate(leaf, reward)
-    
-    def best_child(self, node, **kwargs):
-        partial_policy = functools.partial(self.bandit_strategy,
-                                           node, **kwargs)
-        return max(node.children, key=partial_policy)
 
-    def _select(self, node):
+    def best_child(self, node):
+        partial_bandit_strategy = functools.partial(self.bandit_strategy, node)
+        return max(node.children, key=partial_bandit_strategy)
+
+    def _tree_policy(self, node):
         while not node.state.terminal:
             if not node.fully_expanded:
                 return node.expand()
             node = self.best_child(node)
         return node
 
-    def _rollout(self, state):
+    def _default_policy(self, state):
         while not state.terminal:
-            action = self.rollout_policy(state)
-            state = state.take_action(action)
+            action = random.choice(state.actions)
+            state = state.step(action)
         return state.reward
 
     def _backpropagate(self, node, reward):
@@ -49,7 +42,7 @@ class MCTS:
             node = node.parent
 
 
-class MCTSNode:
+class Node:
 
     def __init__(self, state, parent=None):
         self.state = state
@@ -58,45 +51,34 @@ class MCTSNode:
         self.visits = self.total_reward = 0
 
     @property
+    def exploitation_component(self):
+        return (self.total_reward / self.visits
+                if self.visits else float("-inf"))
+
+    @property
     def fully_expanded(self):
         return len(self.children) == len(self.state.actions)
 
     def expand(self):
-        action = self._next_action()
-        child = self.take_action(action, children=False)
+        action = self._find_action()
+        child = self._take_action(action)
         self.children.add(child)
         return child
-
-    def _next_action(self):
-        degree = len(self.children)
-        return self.state.actions[degree]
 
     def update(self, reward):
         self.visits += 1
         self.total_reward += reward[self.state.agent]
 
-    def take_action(self, action, children=True):
-        if children:
-            for child in self.children:
-                if child.state.action == action:
-                    return child
+    def _find_action(self):
+        degree = len(self.children)
+        return self.state.actions[degree]
+
+    def _take_action(self, action):
         state = self.state.take_action(action)
-        return MCTSNode(state, parent=self)
+        return Node(state, parent=self)
 
 
-class MCTSState(abc.ABC):
-
-    @property
-    @abc.abstractmethod
-    def agent(self): ...
-
-    @property
-    @abc.abstractmethod
-    def reward(self): ...
-
-    @property
-    @abc.abstractmethod
-    def terminal(self): ...
+class State(abc.ABC):
 
     @property
     @abc.abstractmethod
@@ -106,5 +88,21 @@ class MCTSState(abc.ABC):
     @abc.abstractmethod
     def actions(self): ...
 
+    @property
     @abc.abstractmethod
-    def take_action(self, action): ...
+    def agent(self): ...
+
+    @property
+    @abc.abstractmethod
+    def observation(self): ...
+
+    @property
+    @abc.abstractmethod
+    def reward(self): ...
+
+    @property
+    @abc.abstractmethod
+    def terminal(self): ...
+
+    @abc.abstractmethod
+    def step(self, action): ...
