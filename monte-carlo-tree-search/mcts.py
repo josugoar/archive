ac77378de3bf4a6b302ee07311eab05f458306ca
Josu <56826352+josugoar@users.noreply.github.com>
2021-04-01 10:45:30 +0200

    Update mcts.py

--- monte-carlo-tree-search/mcts.py
+++ monte-carlo-tree-search/mcts.py
@@ -1,10 +1,10 @@
 import abc
 import math
 import random
-# https://www.scitepress.org/papers/2018/66535/66535.pdf
+
 
 def uct(node, exploration_weight=math.sqrt(2)):
-    return (node.exploitation_component + exploration_weight
+    return (node.total_reward / node.visits + exploration_weight
             * math.sqrt(math.log(node.parent.visits) / node.visits))
 
 
@@ -20,18 +20,19 @@ class MCTS:
 
     def search(self, root):
         leaf = self._select(root)
-        # TODO: Add inline expansion phase
-        reward = self._simulate(leaf.state)
-        self._backpropagate(leaf, reward)
-
-    def best_child(self, node):
-        return max(node.children, key=self.tree_policy)
+        child = self._expand(leaf)
+        reward = self._simulate(child.state)
+        self._backpropagate(child, reward)
 
     def _select(self, node):
-        while not node.state.terminal:
-            if not node.fully_expanded:
-                return node.expand()
-            node = self.tree_policy(node)
+        while node.fully_expanded and not node.state.terminal:
+            node = max(node.children, key=self.tree_policy)
+        return node
+
+    def _expand(self, node):
+        if not node.state.terminal:
+            action = node.choose()
+            return node.add_child(action)
         return node
 
     def _simulate(self, state):
@@ -55,31 +56,22 @@ class Node:
         self.visits = self.total_reward = 0
 
     @property
-    def exploitation_component(self):
-        return (self.total_reward / self.visits
-                if self.visits else float("-inf"))
-
-    @property
     def fully_expanded(self):
         return len(self.children) == len(self.state.actions)
 
-    def expand(self):
-        action = self._find_action()
-        child = self._take_action(action)
+    def add_child(self, action):
+        state = self.state.step(action)
+        child = Node(state, parent=self)
         self.children.add(child)
         return child
 
-    def update(self, reward):
-        self.visits += 1
-        self.total_reward += reward[self.state.agent]
-
-    def _find_action(self):
+    def choose(self):
         degree = len(self.children)
         return self.state.actions[degree]
 
-    def _take_action(self, action):
-        state = self.state.take_action(action)
-        return Node(state, parent=self)
+    def update(self, reward):
+        self.visits += 1
+        self.total_reward += reward
 
 
 class State(abc.ABC):
@@ -91,14 +83,6 @@ class State(abc.ABC):
     @property
     @abc.abstractmethod
     def actions(self): ...
-
-    @property
-    @abc.abstractmethod
-    def agent(self): ...
-
-    @property
-    @abc.abstractmethod
-    def observation(self): ...
 
     @property
     @abc.abstractmethod
