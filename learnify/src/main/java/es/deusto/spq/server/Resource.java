e87cabc9ac24229f903126380d6d2e1ac650b7f9
gaizka-basterra <gaizka.basterra@opendeusto.es>
2023-04-02 19:37:43 +0200

    Create update and modify register methods

--- learnify/src/main/java/es/deusto/spq/server/Resource.java
+++ learnify/src/main/java/es/deusto/spq/server/Resource.java
@@ -14,8 +14,11 @@ import es.deusto.spq.pojo.UserData;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -81,12 +84,14 @@ public class Resource {
 		}
 	}
 
-	@POST
-	@Path("/register")
-	public Response registerUser(UserData userData) {
+	@PUT
+    @Path("/{login}/update")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateUser(@PathParam("login") String login, UserData userData) {
 		try {
 			tx.begin();
-			logger.info("Checking whether the user already exits or not: '{}'", userData.getLogin());
+			logger.info("Checking whether the user already exists or not: '{}'", userData.getLogin());
 			User user = null;
 			try {
 				user = pm.getObjectById(User.class, userData.getLogin());
@@ -98,14 +103,58 @@ public class Resource {
 				logger.info("Setting password user: {}", user);
 				user.setPassword(userData.getPassword());
 				logger.info("Password set user: {}", user);
+
+				logger.info("Setting password user: {}", user);
+				user.setName(userData.getName());
+				logger.info("Password set user: {}", user);
+
+				logger.info("Setting surname user: {}", user);
+				user.setSurname(userData.getSurname());
+				logger.info("Surname set user: {}", user);
+
+				tx.commit();
+				return Response.status(Status.OK).build();
+			} else {
+				logger.info("The user does not exist");
+
+				tx.commit();
+				return Response.status(Status.BAD_REQUEST).build();
+			}
+		} finally {
+			if (tx.isActive()) {
+				tx.rollback();
+			}
+		}
+    }
+
+
+	@POST
+	@Path("/register")
+	public Response registerUser(UserData userData) {
+		try {
+			tx.begin();
+			logger.info("Checking whether the user already exists or not: '{}'", userData.getLogin());
+			User user = null;
+			try {
+				user = pm.getObjectById(User.class, userData.getLogin());
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				logger.info("Exception launched: {}", jonfe.getMessage());
+			}
+			logger.info("User: {}", user);
+			if (user != null) {
+				logger.info("The user already exists");
+
+				tx.commit();
+				return Response.status(Status.BAD_REQUEST).build();
 			} else {
 				logger.info("Creating user: {}", user);
 				user = new User(userData.getLogin(), userData.getPassword(), userData.getName(), userData.getSurname());
 				pm.makePersistent(user);
 				logger.info("User created: {}", user);
+
+				tx.commit();
+				return Response.status(Status.OK).build();
 			}
-			tx.commit();
-			return Response.ok().build();
 		} finally {
 			if (tx.isActive()) {
 				tx.rollback();
