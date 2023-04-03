49fa8b3ca72b8cd4772421c32c4ca1f426c0326e
julencasazk <julen.casal@opendeusto.es>
2023-04-03 08:30:47 +0200

    Reformat code

--- learnify/src/main/java/es/deusto/spq/client/WindowDashboard.java
+++ learnify/src/main/java/es/deusto/spq/client/WindowDashboard.java
@@ -65,10 +65,6 @@ public class WindowDashboard extends JFrame {
 	
 	public WindowDashboard() {
 		
-		
-		
-		
-		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1280, 720);
 		contentPane = new JPanel();
