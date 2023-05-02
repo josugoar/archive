f75f82efd936859cedd44c46b310255bc0a02c34
Aïnhoa Egia <ainhoa.egia@opendeusto.es>
2023-05-02 22:07:23 +0200

    Tested ScoreData

--- learnify/src/test/java/es/deusto/spq/pojo/ScoreDataTest.java
+++ learnify/src/test/java/es/deusto/spq/pojo/ScoreDataTest.java
@@ -1,5 +1,46 @@
 package es.deusto.spq.pojo;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class ScoreDataTest {
+    ScoreData scoreData;
+    UserData student;
+    SubjectData subject;
     
+    @Before
+    public void setUp() {
+        scoreData = new ScoreData();
+        scoreData.setId(0000);
+        scoreData.setScore(5.0f);
+        scoreData.setStudent(student);
+        scoreData.setSubject(subject);
+    }
+
+    @Test
+    public void testGetID() {
+        assertEquals(0000, scoreData.getId().intValue());
+    }
+
+    @Test
+    public void testGetScore() {
+        assertEquals(5.0f, scoreData.getScore().floatValue(), 0f);
+    }
+
+    @Test
+    public void testGetStudent() {
+        assertEquals(student, scoreData.getStudent());
+    }
+
+    @Test
+    public void testGetSubject() {
+        assertEquals(subject, scoreData.getSubject());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("[Score: user name = student.getName(), user surname = student.getSurname(), subject = subject, score = 5.0f]", scoreData.toString());
+    }
 }
