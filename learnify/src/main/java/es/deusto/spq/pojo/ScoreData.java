385ae9e870083649fd532f3e0f8ff9a0c9a368ba
josugoar <jogoar@opendeusto.es>
2023-05-03 09:51:28 +0200

    Small refactor

--- learnify/src/main/java/es/deusto/spq/pojo/ScoreData.java
+++ learnify/src/main/java/es/deusto/spq/pojo/ScoreData.java
@@ -8,15 +8,14 @@ public class ScoreData {
     private Float score;
     private Integer id;
 
+    public ScoreData(){
+    }
+
     public ScoreData(Score score) {
-        // required for serialization
         this.subject = new SubjectData(score.getSubject());
         this.student = new UserData(score.getStudent());
         this.score = score.getScore();
         this.id = score.getId();
-    }
-
-    public ScoreData(){
     }
 
     public SubjectData getSubject() {
@@ -55,4 +54,48 @@ public class ScoreData {
 		return "Score: user name --> " + this.student.getName() + ", user surname --> " + this.student.getSurname() + ", subject -->" + this.subject
 				+ ", score -->  " + this.score + "]";
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((subject == null) ? 0 : subject.hashCode());
+        result = prime * result + ((student == null) ? 0 : student.hashCode());
+        result = prime * result + ((score == null) ? 0 : score.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ScoreData other = (ScoreData) obj;
+        if (subject == null) {
+            if (other.subject != null)
+                return false;
+        } else if (!subject.equals(other.subject))
+            return false;
+        if (student == null) {
+            if (other.student != null)
+                return false;
+        } else if (!student.equals(other.student))
+            return false;
+        if (score == null) {
+            if (other.score != null)
+                return false;
+        } else if (!score.equals(other.score))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
--- learnify/src/main/java/es/deusto/spq/server/jdo/Subject.java
+++ learnify/src/main/java/es/deusto/spq/server/jdo/Subject.java
@@ -8,11 +8,11 @@ import javax.jdo.annotations.PrimaryKey;
 @PersistenceCapable
 public class Subject {
     
+    @PrimaryKey
+    private Integer id = null;
     private Date startDate = null;
     private String name = null;
     private User professor = null;
-    @PrimaryKey
-    private Integer id = null;
 
     public Subject() {
 	}
@@ -53,5 +53,48 @@ public class Subject {
 		return "Subject: name --> " + this.name + ", start date --> " + this.startDate + ", professor name -->" + this.professor.getName()
 				+ ", professor surname -->" + this.professor.getSurname() +", id -->  " + this.id + "]";
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((startDate == null) ? 0 : startDate.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((professor == null) ? 0 : professor.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Subject other = (Subject) obj;
+        if (startDate == null) {
+            if (other.startDate != null)
+                return false;
+        } else if (!startDate.equals(other.startDate))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (professor == null) {
+            if (other.professor != null)
+                return false;
+        } else if (!professor.equals(other.professor))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
