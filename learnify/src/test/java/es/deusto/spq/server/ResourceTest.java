9e484f99449cdf4d8f5fc2419c0a4f4f49639773
josugoar <jogoar@opendeusto.es>
2023-05-25 16:01:29 +0200

    Format

--- learnify/src/test/java/es/deusto/spq/server/ResourceTest.java
+++ learnify/src/test/java/es/deusto/spq/server/ResourceTest.java
@@ -33,7 +33,6 @@ import es.deusto.spq.server.jdo.Score;
 import es.deusto.spq.server.jdo.Subject;
 import es.deusto.spq.server.jdo.User;
 
-
 public class ResourceTest {
 
     private Resource resource;
