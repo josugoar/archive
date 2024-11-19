1d3b3b17ac43f26b3168c13873834aa772919438
Josu <56826352+josugoar@users.noreply.github.com>
2024-11-19 17:57:17 +0100

    Delete dii/oracle directory

--- miot/dii/oracle/contracts/BasicOracle.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
-
-import {OracleBase} from "./OracleBase.sol";
-
-contract BasicOracle is OracleBase {
-    constructor() OracleBase(msg.sender) {}
-}
