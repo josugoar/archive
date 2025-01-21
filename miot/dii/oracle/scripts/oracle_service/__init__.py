d311c20e997295cb137792ce1451552ea15ab151
Josu <56826352+josugoar@users.noreply.github.com>
2025-01-21 03:09:43 +0100

    Remove deprecated Oracle service components and clean up related files

--- miot/dii/oracle/scripts/oracle_service/__init__.py
+++
@@ -1,4 +0,0 @@
-from .events import OracleEvents
-from .oracle_service import oracle_service
-
-__all__ = ['OracleEvents', 'oracle_service']
--- miot/dii/oracle/scripts/oracle_service/events.py
+++
@@ -1,6 +0,0 @@
-import enum
-
-
-class OracleEvents(enum.StrEnum):
-    SENT_REQUEST = "SentRequest"
-    CANCELLED_REQUEST = "CancelledRequest"
